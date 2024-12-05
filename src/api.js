18940b42274b17658e95bb75d508ca134d2dcbbe
Joshua <jhellst@gmail.com>
2024-12-05 12:43:28 -0800

    Minor code cleanup - removed unnecessary logging statements and unused routes.

--- src/api.js
+++ src/api.js
@@ -18,15 +18,13 @@ class SoccerLeaguesApi {
       'Access-Control-Allow-Origin': `${BASE_URL}`,
     };
 
-    // set to undefined since the body property cannot exist on a GET method.
+    // Set to undefined since the body property cannot exist on a GET method.
     const body = (method !== "GET")
       ? JSON.stringify(data)
       : undefined;
     const resp = await fetch(url, { method, body, headers });
 
-    //fetch API does not throw an error, have to dig into the resp for msgs
     if (!resp.ok) {
-      // console.error("API Error:", resp.statusText, resp.status);
       const { error } = await resp.json();
       throw Array.isArray(error) ? error : [error];
     }
@@ -37,7 +35,6 @@ class SoccerLeaguesApi {
   /** Get all leagues in database. */
   static async getLeagues() {
     let leagues = await this.request(`leagues`);
-    // console.log("leagues@App", leagues);
     return leagues;
   }
 
